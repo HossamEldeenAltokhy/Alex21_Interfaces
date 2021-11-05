56294d6138108834446ef57861deb0152552075b
HossamEldeenAltokhy <hossamaltokhy@gmail.com>
2021-11-05 13:43:38 +0200

    update TWI

---
+++ mTWI.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   mTWI.h
+ * Author: hossa
+ *
+ * Created on November 5, 2021, 12:52 PM
+ */
+
+#ifndef MTWI_H
+#define	MTWI_H
+
+
+#define mAddress   0x06
+#define SLA_W      0x02   //00000010
+#define SLA_R      0x03   //00000011
+
+int  TWI_start(char address);
+void TWI_stop();
+int  TWI_write(char data);
+char TWI_read();
+
+///////////////
+
+
+void TWI_init_slave(char address);
+void TWI_slave_listen();
+int  TWI_slave_write(char data);
+char TWI_slave_read();
+
+
+
+
+
+#endif	/* MTWI_H */
+
--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c IO.c uart.c mLCD.c mLCD_4bit.c mMotor.c mKeypad.c mExternalInterrupt.c mADC.c mTimer.c mSPI.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c IO.c uart.c mLCD.c mLCD_4bit.c mMotor.c mKeypad.c mExternalInterrupt.c mADC.c mTimer.c mSPI.c mTWI.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/IO.o ${OBJECTDIR}/uart.o ${OBJECTDIR}/mLCD.o ${OBJECTDIR}/mLCD_4bit.o ${OBJECTDIR}/mMotor.o ${OBJECTDIR}/mKeypad.o ${OBJECTDIR}/mExternalInterrupt.o ${OBJECTDIR}/mADC.o ${OBJECTDIR}/mTimer.o ${OBJECTDIR}/mSPI.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/IO.o.d ${OBJECTDIR}/uart.o.d ${OBJECTDIR}/mLCD.o.d ${OBJECTDIR}/mLCD_4bit.o.d ${OBJECTDIR}/mMotor.o.d ${OBJECTDIR}/mKeypad.o.d ${OBJECTDIR}/mExternalInterrupt.o.d ${OBJECTDIR}/mADC.o.d ${OBJECTDIR}/mTimer.o.d ${OBJECTDIR}/mSPI.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/IO.o ${OBJECTDIR}/uart.o ${OBJECTDIR}/mLCD.o ${OBJECTDIR}/mLCD_4bit.o ${OBJECTDIR}/mMotor.o ${OBJECTDIR}/mKeypad.o ${OBJECTDIR}/mExternalInterrupt.o ${OBJECTDIR}/mADC.o ${OBJECTDIR}/mTimer.o ${OBJECTDIR}/mSPI.o ${OBJECTDIR}/mTWI.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/IO.o.d ${OBJECTDIR}/uart.o.d ${OBJECTDIR}/mLCD.o.d ${OBJECTDIR}/mLCD_4bit.o.d ${OBJECTDIR}/mMotor.o.d ${OBJECTDIR}/mKeypad.o.d ${OBJECTDIR}/mExternalInterrupt.o.d ${OBJECTDIR}/mADC.o.d ${OBJECTDIR}/mTimer.o.d ${OBJECTDIR}/mSPI.o.d ${OBJECTDIR}/mTWI.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/IO.o ${OBJECTDIR}/uart.o ${OBJECTDIR}/mLCD.o ${OBJECTDIR}/mLCD_4bit.o ${OBJECTDIR}/mMotor.o ${OBJECTDIR}/mKeypad.o ${OBJECTDIR}/mExternalInterrupt.o ${OBJECTDIR}/mADC.o ${OBJECTDIR}/mTimer.o ${OBJECTDIR}/mSPI.o
+OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/IO.o ${OBJECTDIR}/uart.o ${OBJECTDIR}/mLCD.o ${OBJECTDIR}/mLCD_4bit.o ${OBJECTDIR}/mMotor.o ${OBJECTDIR}/mKeypad.o ${OBJECTDIR}/mExternalInterrupt.o ${OBJECTDIR}/mADC.o ${OBJECTDIR}/mTimer.o ${OBJECTDIR}/mSPI.o ${OBJECTDIR}/mTWI.o
 
 # Source Files
-SOURCEFILES=main.c IO.c uart.c mLCD.c mLCD_4bit.c mMotor.c mKeypad.c mExternalInterrupt.c mADC.c mTimer.c mSPI.c
+SOURCEFILES=main.c IO.c uart.c mLCD.c mLCD_4bit.c mMotor.c mKeypad.c mExternalInterrupt.c mADC.c mTimer.c mSPI.c mTWI.c
 
 
 
@@ -160,6 +160,12 @@ ${OBJECTDIR}/mSPI.o: mSPI.c  .generated_files/flags/default/39535a98cafa17019943
 	@${RM} ${OBJECTDIR}/mSPI.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1 -g -DDEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1 -gdwarf-2  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -funsigned-char -funsigned-bitfields -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3     -MD -MP -MF "${OBJECTDIR}/mSPI.o.d" -MT "${OBJECTDIR}/mSPI.o.d" -MT ${OBJECTDIR}/mSPI.o -o ${OBJECTDIR}/mSPI.o mSPI.c 
 	
+${OBJECTDIR}/mTWI.o: mTWI.c  .generated_files/flags/default/b0299a11f6cfec31f1bb7401348df2bc27276bcc .generated_files/flags/default/2f9e58fd8ce1a2611b0e0fd33168db129b9285f3
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/mTWI.o.d 
+	@${RM} ${OBJECTDIR}/mTWI.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1 -g -DDEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1 -gdwarf-2  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -funsigned-char -funsigned-bitfields -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3     -MD -MP -MF "${OBJECTDIR}/mTWI.o.d" -MT "${OBJECTDIR}/mTWI.o.d" -MT ${OBJECTDIR}/mTWI.o -o ${OBJECTDIR}/mTWI.o mTWI.c 
+	
 else
 ${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/7b3a5bb213d609c3714a478d41ea4bf35f4b6bd0 .generated_files/flags/default/2f9e58fd8ce1a2611b0e0fd33168db129b9285f3
 	@${MKDIR} "${OBJECTDIR}" 
@@ -226,6 +232,12 @@ ${OBJECTDIR}/mSPI.o: mSPI.c  .generated_files/flags/default/27aa8833340abec3d652
 	@${RM} ${OBJECTDIR}/mSPI.o.d 
 	@${RM} ${OBJECTDIR}/mSPI.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -funsigned-char -funsigned-bitfields -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3     -MD -MP -MF "${OBJECTDIR}/mSPI.o.d" -MT "${OBJECTDIR}/mSPI.o.d" -MT ${OBJECTDIR}/mSPI.o -o ${OBJECTDIR}/mSPI.o mSPI.c 
+	
+${OBJECTDIR}/mTWI.o: mTWI.c  .generated_files/flags/default/f86d37a65518808cc35f41ca60581a054d44c1f2 .generated_files/flags/default/2f9e58fd8ce1a2611b0e0fd33168db129b9285f3
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/mTWI.o.d 
+	@${RM} ${OBJECTDIR}/mTWI.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -x c -D__$(MP_PROCESSOR_OPTION)__   -mdfp="${DFP_DIR}/xc8"  -Wl,--gc-sections -O1 -ffunction-sections -fdata-sections -fshort-enums -funsigned-char -funsigned-bitfields -Wall -DXPRJ_default=$(CND_CONF)  $(COMPARISON_BUILD)  -gdwarf-3     -MD -MP -MF "${OBJECTDIR}/mTWI.o.d" -MT "${OBJECTDIR}/mTWI.o.d" -MT ${OBJECTDIR}/mTWI.o -o ${OBJECTDIR}/mTWI.o mTWI.c 
 	
 endif
 
